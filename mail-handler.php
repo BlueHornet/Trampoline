d22727558cdf006375e13c2c61c5040e45bfc8cd
Randall Kahler <randy@bluehornet.com>
2015-08-20 12:42:51 -0700

    Adding new relay/maildir PHP handlers; moving original handler to .py file; adding gitignore

--- mail-handler.php
+++ mail-handler.php
@@ -15,4 +15,10 @@ if (!file_exists('php-handlers/'.$script)) {
     exit("Bad recipient or no recipient found");
 }
 
+// Read configuration
+if (!is_file('config.ini')) {
+    exit("No config file available");
+}
+$config = parse_ini_file("config.ini", true);
+
 require_once('php-handlers/'.$script);
